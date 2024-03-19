6cbb629dd7bc2c5528ec7a8c6f506b914acb1431
Augusto Del Grosso <adg.prodlp@gmail.com>
2024-03-19 09:56:57 -0300

    Remove unused typography variants

--- components/Typography/index.tsx
+++ components/Typography/index.tsx
@@ -1,15 +1,11 @@
 import Body from "./variants/Body";
 import Body2 from "./variants/Body2";
-import Caption from "./variants/Caption";
 import H1 from "./variants/H1";
 import H2 from "./variants/H2";
 import H3 from "./variants/H3";
 import H4 from "./variants/H4";
 import H5 from "./variants/H5";
-import Subtitle from "./variants/Subtitle";
-import Subtitle2 from "./variants/Subtitle2";
-import Overline from "./variants/Overline";
-import Button from "./variants/Button";
+
 const Typography = {
   H1,
   H2,
