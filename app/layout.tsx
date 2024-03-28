1de861890b5b18f6617d78078dffc68c5499ce38
Augusto Del Grosso <adg.prodlp@gmail.com>
2024-03-28 18:31:35 -0300

    Add Experience section (#3)
    * Add astronaut image, update homepage layout, fix responsive and change astronaut.png to webp
    
    * Refactor NavbarItems component to add hover effect to buttons
    
    * Refactor NavbarItems component to use fragment
    
    * Update package.json name
    
    * Fix merges
    
    * Fix aboutme responsive
    
    * add exoerience section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import { Navbar } from "@/components/Navbar";
+import Typography from "@/components/Typography";
 import { fontSans } from "@/config/fonts";
 import { siteConfig } from "@/config/site";
 import "@/styles/globals.css";
 import clsx from "clsx";
 import { Metadata } from "next";
+import Link from "next/link";
 import { Providers } from "./providers";
 
 export const metadata: Metadata = {
@@ -28,10 +30,12 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
-  
   return (
-    <html lang="en" className={`${fontSans.className} scroll-smooth`} suppressHydrationWarning>
+    <html
+      lang="es"
+      className={`${fontSans.className} scroll-smooth`}
+      suppressHydrationWarning
+    >
       <head />
       <body
         className={clsx(
@@ -40,11 +44,23 @@ export default function RootLayout({
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
-          <Navbar />
           <div id="home" className=" h-full w-full  ">
-            <main className="relative flex flex-col w-screen mx-auto ">{children}</main>
+            <Navbar />
+            <main className="relative flex flex-col w-screen mx-auto ">
+              {children}
+            </main>
           </div>
         </Providers>
+      <footer>
+        <div className="mb-10 mx-auto flex flex-col align-middle justify-center text-center">
+          <Typography.Body className="text-small text-gray-500">
+            Designed and developed by Taca 👨‍💻
+          </Typography.Body>
+          <Link href={"https://github.com/augustodelg/portfolio-next"} target="_blank" className="text-gray-500">
+            Version 0.0.2
+          </Link>
+        </div>
+      </footer>
       </body>
     </html>
   );
---
+++ components/Timeline/types.ts
@@ -0,0 +1,10 @@
+export type TimelineItemProps = {
+    children: React.ReactNode;
+    date?: React.ReactNode | string;
+    title?: React.ReactNode | string;
+    description?: React.ReactNode | string;
+    point?: React.ReactNode;
+    subtitle?: React.ReactNode | string;
+    current?: boolean
+  };
+  
